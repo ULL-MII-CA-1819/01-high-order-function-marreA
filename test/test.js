2df1501d5158cb183f3ae045aeeb6d0a4609c684
marreA <alemarrerodiaz@gmail.com>
2018-10-05 09:47:44 +0100

    Ejercicio 3 añadido. Pruebas pasadas correctamente

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 // Cargamos el primer ejercicio
 const flat = require("../flattening.js");
 const loop = require("../loop.js");
-
+const every = require("../everything.js");
 const assert = require('assert');
 
 describe('Chapter 5 - High Order Functions', () => {
@@ -13,14 +13,25 @@ describe('Chapter 5 - High Order Functions', () => {
     });
 });
 
-describe('Chapter 5 - High Order Functions', () => {
+/* describe('Chapter 5 - High Order Functions', () => {
     describe('Loop', () => {
         it('Value is [1] after loop', () => {
             let values = [1, 2, 3, 4];
             loop(values, values => values.length >= 1, values => values = values.slice(0, values.length - 1), values => console.log(values[values.length - 1]));
             assert.deepEqual(values, [1]);
+        }); 
+    });
+}); */
+
+describe('Chapter 5 - High Order Functions', () => {
+    describe('Every', () => {
+        it('Every returns true', () => {
+            let array = [5, 6, 7, 8, 9, 10];
+            assert.equal(every(array, (element) => element > 2), true);
+        });
+        it('Every returns false', () => {
+            let array = [1, 6, 7, 8, 9, 10];
+            assert.equal(every(array, (element) => element > 2), false);
         });
     });
 });
-
-
