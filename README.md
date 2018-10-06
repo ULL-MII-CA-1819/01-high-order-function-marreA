9a79ffe7f26adde81e4c79ae97906ceef669e6a2
Iván García <alu0100693737@ull.edu.es>
2018-10-06 21:09:14 +0200

    Pequeños arreglos

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Práctica: Higher-Order Functions. Colaboración
 
 [Enlace a la web de la práctica](https://ull-mii-ca-1819.github.io/01-high-order-function-marreA/)
+
+- nodejs
+- Gulp
+- Mocha - Chai
+- Asciidoctor
+(Travis pending)
+blanket, istanbul problems
--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,6 @@ gulp.task("test", () =>
         gulp.src('./docs/test/testNode.js', {read: false})
         .pipe(mocha({reporter: 'nyan'}))
 );
-// gulp.task("test", shell.task("mocha"));
 
 gulp.task('docs', shell.task("asciidoctor docs/resumen.adoc -o docs/index.html"));
 
@@ -21,8 +20,6 @@ gulp.task("pre-install", shell.task([
     ]));
 
 gulp.task('minify', function () {
-
-
     gulp.src('./docs/index.html')
     .pipe(minifyHTML())
     .pipe(gulp.dest('./minified/'))
